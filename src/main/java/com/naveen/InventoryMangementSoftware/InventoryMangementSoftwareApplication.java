7079ec0059c0789cd3e8b566b6af3f35b0711b11
Naveen Kumar <naveenkumar16613@gmail.com>
2025-04-27 17:48:14 +0530

    initial commit

---
+++ src/main/java/com/naveen/InventoryMangementSoftware/InventoryMangementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.InventoryMangementSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMangementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMangementSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveen/InventoryMangementSoftware/InventoryMangementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.InventoryMangementSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMangementSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
